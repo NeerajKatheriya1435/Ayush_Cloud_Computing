29f2d99df910832016110e18b71018a7c2f966ff
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-07 20:57:41 +0530

    Initial Commit and Syllabus

---
+++ day1.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Starting backup..."
+Copy-Item "D:\Neeraj Notes\Cloud Computing Notes\Cloud Computing PDF Notes" -Destination  "D:\Neeraj Notes\Cloud Computing Notes\New Folder" -Recurse
+Write-Host "eeBackup completed successfully!"
