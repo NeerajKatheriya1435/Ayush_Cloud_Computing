c59baf9273261cef525830e75e90f725ce3aa8be
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-14 20:51:13 +0530

    Networking Commands

--- day1.ps1
+++
@@ -1,3 +0,0 @@
-﻿Write-Host "Starting backup..."
-Copy-Item "D:\Neeraj Notes\Cloud Computing Notes\Cloud Computing PDF Notes" -Destination  "D:\Neeraj Notes\Cloud Computing Notes\New Folder" -Recurse
-Write-Host "eeBackup completed successfully!"
