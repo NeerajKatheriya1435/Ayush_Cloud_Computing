e8e4bf3facdf8c4266f05bfc31db156a3f7b11c6
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-08-15 20:27:51 +0530

    Chapter 2 and scripting started

---
+++ PowerShell Scripting Language/day1 Power Script.ps1
@@ -0,0 +1,50 @@
+﻿
+
+# [int] $num1=89;
+
+# 45 23 68.56  -78 
+# 45.74548 56.90 89454.8754
+# Rohan Shivam John Peter
+#
+# $arr = @(1,2,3,4,454,5,45,5,56,56,6)
+# write-host $arr
+# $hash = @{Name="John Peter"; Age=25; Salary=56000}
+
+# $num="Hello World";
+
+# Write-Host $num;
+
+# [int] $a=67;
+
+# Write-Host $($a -ne 89);
+
+# [string] $name="Rohan" 
+# Write-Host $name
+
+# $marks=@(2,5,7,8,8)
+
+# Write-Host $marks
+
+# $person=@{name="Rahul Kumar";age=78;salary=5600;con=12234566787}
+# Write-Host $person.salary
+
+# Write-Host $a.GetType().Name
+
+#  5-6
+
+$num1=21;
+$num2=8;
+
+# $result=$num1-$num2
+
+#$result=$num1%$num2
+
+#Write-Host $result;
+#$num1++;
+#$num1++;
+
+# Write-Host "Message" $num1;
+
+Write-Host ($num1 -eq $num2)
+Write-Host ($num1 -ne $num2)
+Write-Host ($num1 -gt $num2)
